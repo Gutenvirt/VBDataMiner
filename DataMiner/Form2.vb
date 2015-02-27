74616765be4a7b67cbcafa79d074af94c7a1f434
Chris Stefancik <cdstefan@volusia.k12.fl.us>
2015-02-27 08:05:54 -0500

    Updated copyright agreement

--- DataMiner/Form2.vb
+++ DataMiner/Form2.vb
@@ -1,6 +1,18 @@
-﻿'2015 Chris Stefancik'
-'Please keep this header when distributing, modifying, or compiling the code'
-'gutenvirt@gmail.com for more information' 
+﻿'VBDataMiner - Extract and analyze data from MS Excel(c) files.
+'Copyright (C) 2015 Chris Stefancik gutenvirt@gmail.com
+
+'This program is free software: you can redistribute it and/or modify
+'it under the terms of the GNU General Public License as published by
+'the Free Software Foundation, either version 3 of the License, or
+'any later version.
+
+'This program is distributed in the hope that it will be useful,
+'but WITHOUT ANY WARRANTY; without even the implied warranty of
+'MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+'GNU General Public License for more details.
+
+'You should have received a copy of the GNU General Public License
+'along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 Option Explicit On
 
